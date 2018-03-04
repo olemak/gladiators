18e42299deac1cfc86445d24455dffedc34422dc
Ole Martin Kristiansen <olemak@apasje.no>
2018-03-04 22:57:11 +0100

    Successful GraphiQL/Apollo query

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const gladiatorQuery = gql`
+    {
+        allGladiators {
+            name
+            description
+            type
+            image {
+                url
+            }
+            attack
+            health
+            defense
+        }
+    }
+`;
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="Gladiators">
+                <h1>Gladiators</h1>
+            </div>
+        );
+    }
 }
 
-export default App;
+export const AppWithData = graphql(gladiatorQuery)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import { AppWithData } from "./App";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+const GRAPHCMS_API = "https://api.graphcms.com/simple/v1/gladiators";
+
+const client = new ApolloClient({
+    link: new HttpLink({ uri: GRAPHCMS_API }),
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <AppWithData />
+    </ApolloProvider>,
+    document.getElementById("root")
+);
 registerServiceWorker();
